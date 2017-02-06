d27ab9823e67f2761a036939d1d8cb70a19b8597
Sebastian Alonso Mao <sebastian.alonso@atrapalo.com>
2017-02-06 18:28:12 +0100

    Call url before each test

--- tests/test_flightHome.js
+++ tests/test_flightHome.js
@@ -8,36 +8,35 @@ describe('Test express', function() {
         driver.ignoreSynchronization = true;
         params = browser.params;
         this.url = params.home.url;
+        driver.get(this.url);
     });
 
     it('formulario',function() {
-        driver.get(this.url).
-            then(function () {
-                driver.findElement(by.id('tabVUE')).
-                    then(function(element) {
-                        expect(element.getText()).not.toBe(null);
-                });
-            }).then(function(){
-                driver.findElement(by.id('origen_vue')).sendKeys(params.home.origin);
-                driver.findElement(by.id('destino_vue')).sendKeys(params.home.destination);
+        driver.findElement(by.id('tabVUE')).
+            then(function(element) {
+                expect(element.getText()).not.toBe(null);
+        }).then(function(){
+            driver.findElement(by.id('origen_vue')).sendKeys(params.home.origin);
+            driver.findElement(by.id('destino_vue')).sendKeys(params.home.destination);
 
-            }).then(function() {
-                driver.findElement(by.id('pasajeros_vue')).getAttribute('value').
-                    then(function(value) {
-                        expect(value).toEqual(params.home.passengers);
-                    });
-            }).then(function() {
-                driver.findElement(by.css('a[href*="search_VUE_class_and_discounts"]')).
-                    then(function(element) {
-                        expect(element.getText()).not.toBe(null);
-                    });
-            }).then(function() {
-                driver.findElement(by.id('li_ida')).click();
-            }).then(function() {
-                driver.findElement(by.id('div_fecha_vuelta_vue')).getAttribute('class').
-                    then(function(classes) {
-                        expect(classes.split(' ').indexOf('disabled')).not.toEqual(-1);
+        }).then(function() {
+            driver.findElement(by.id('pasajeros_vue')).getAttribute('value').
+                then(function(value) {
+                    expect(value).toEqual(params.home.passengers);
                 });
+        }).then(function() {
+            driver.findElement(by.css('a[href*="search_VUE_class_and_discounts"]')).
+                then(function(element) {
+                    expect(element.getText()).not.toBe(null);
+                });
+        }).then(function() {
+            driver.findElement(by.id('li_ida')).click();
+        }).then(function() {
+            driver.findElement(by.id('div_fecha_vuelta_vue')).getAttribute('class').
+                then(function(classes) {
+                    expect(classes.split(' ').indexOf('disabled')).not.toEqual(-1);
             });
+        });
     });
+
 });
